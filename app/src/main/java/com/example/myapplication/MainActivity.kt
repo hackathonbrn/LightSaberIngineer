4cfaddb0f18bf58e820760ca2fabd02b8ad5ef36
BlackfiremasterX <misharozental@gmail.com>
2021-05-06 17:31:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.unity3d.player.UnityPlayerActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.textView).setOnClickListener {
+            startActivity(Intent(this, UnityPlayerActivity::class.java))
+        }
+    }
+}
