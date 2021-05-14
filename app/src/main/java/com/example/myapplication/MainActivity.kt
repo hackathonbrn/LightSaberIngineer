462f736bfe62f2470db9bb454f9c5b6cfce1723d
arbonik <zmear@mail.ru>
2021-05-15 01:59:08 +0700

    add navigation_nest.xml

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,24 +1,11 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.ui.MainFragment
-import com.unity3d.player.UnityPlayerActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.fragment, MainFragment())
-            .commit()
-
-        findViewById<TextView>(R.id.textView).setOnClickListener {
-            startActivity(Intent(this, UnityPlayerActivity::class.java))
-        }
     }
 }
