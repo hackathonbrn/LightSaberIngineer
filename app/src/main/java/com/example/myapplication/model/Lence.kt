9ce84e11d286c4a6dee9d2b483e9aa51bb335b9b
polina1441 <14042003dp@mail.ru>
2021-05-15 08:16:54 +0700

    add Circuit

--- app/src/main/java/com/example/myapplication/model/Lence.kt
+++ app/src/main/java/com/example/myapplication/model/Lence.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication.model
 
+import com.example.myapplication.componentsView.NestType
+
 class Lence(
     override val name: String
 ) : BaseComponent() {
-
+    override val type: NestType = NestType.EMITTER
 }
 
