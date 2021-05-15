0ea6cc07dbe90b1b8286f8dba5a657fdc19123ee
polina1441 <14042003dp@mail.ru>
2021-05-16 00:30:15 +0700

    add element

--- app/src/main/java/com/example/myapplication/model/Repository.kt
+++ app/src/main/java/com/example/myapplication/model/Repository.kt
@@ -17,18 +17,31 @@ class Repository {
         ),
         BatteryItem(
             Battery(
+                "B320",
+                430.0
+            ),
+            R.drawable.but_big
+        ),
+        BatteryItem(
+            Battery(
                 "B220",
-                200.0
+                300.0
             ),
             R.drawable.but50
         ),
         BatteryItem(
             Battery(
-                "B320",
-                300.0
+                "B120-max",
+                140.0
+            ),
+            R.drawable.but_small
+        ),
+        BatteryItem(
+            Battery(
+                "B120",
+                100.0
             ),
             R.drawable.but100
-
         )
     )
     val lights = listOf(
@@ -37,22 +50,49 @@ class Repository {
                 "E1220",
                 150.0
             ),
-            R.drawable.lense_on
+            R.drawable.emitter_blue
         ),
         LightItem(
             Emitter(
                 "E12",
                 250.0
             ),
-            R.drawable.lense_on
+            R.drawable.emitter_lamp
+        ),
+        LightItem(
+            Emitter(
+                "E-S",
+                350.0
+            ),
+            R.drawable.emitter_sss
         ),
         LightItem(
             Emitter(
                 "E1",
                 350.0
             ),
-            R.drawable.lense_off
-        )
+            R.drawable.emitter_rose
+        ),LightItem(
+            Emitter(
+                "E6",
+                310.0
+            ),
+            R.drawable.emitter_brown
+        ),
+        LightItem(
+            Emitter(
+                "E42",
+                200.0
+            ),
+            R.drawable.emitter_one_big
+        ),
+        LightItem(
+            Emitter(
+                "E1",
+                350.0
+            ),
+            R.drawable.emitter_two
+        ),
     )
 //    var laserSaber = LaserSaber()
 }
